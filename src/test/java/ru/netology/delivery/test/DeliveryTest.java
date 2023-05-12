b469fa7798060bcfd89164fe579d4597f8e0e896
Ilya Bobrov <i.bobrov87@gmail.com>
2023-05-12 08:35:50 +0300

    Corrected by comments

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -42,12 +42,6 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
-
 
         city.setValue(validUser.getCity());
         date.sendKeys(Keys.chord(Keys.CONTROL,"a"), Keys.BACK_SPACE);
@@ -70,6 +64,5 @@ class DeliveryTest {
         successPopUp
                 .shouldBe(visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.text(successMessage + secondMeetingDate));
-
     }
 }
